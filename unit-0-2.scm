51498cdba7464b3199156ea86633f580b120a499
Ben Tillett <ben@myrecovery.ai>
2017-03-13 20:40:39 +0000

    complete unit 0.2

--- unit-0-2.scm
+++ unit-0-2.scm
@@ -12,3 +12,16 @@
   (word
    (item 1 (item 1 sentence))
    (item 1 (item 2 sentence))))
+
+(define (indef-article word)
+  (se
+   (cond
+    ((equal? (first word) 'a) 'an)
+    ((equal? (first word) 'e) 'an)
+    ((equal? (first word) 'i) 'an)
+    ((equal? (first word) 'o) 'an)
+    ((equal? (first word) 'u) 'an)
+    (else 'a)
+    )
+   word
+ ))
