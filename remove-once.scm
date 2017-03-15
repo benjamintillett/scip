c38f8b9d1fef3797abfb53fc64effd4796d41429
Ben Tillett <ben@myrecovery.ai>
2017-03-15 08:51:55 +0000

    complete differences method

--- remove-once.scm
+++ remove-once.scm
@@ -3,3 +3,9 @@
    ((empty? sent) ())
    ((equal? word (first sent)) (bf sent))
    (else (se (first sent) (remove-once word (bf sent))))))
+
+
+(define (differences sent)
+  (if (<= (length sent) 1)
+      ()
+      (se (- (item 1 sent) (item 2 sent)) (differences (bf sent)))))
