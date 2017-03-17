46e224f8b31a298620503829f206c43f45a3340d
Ben Tillett <ben@myrecovery.ai>
2017-03-17 18:26:24 +0000

    encapsulate helper functions in scope of largest-square

--- largest-square.scm
+++ largest-square.scm
@@ -1,23 +1,20 @@
 (load "~/dev/sicp/test-helpers.scm")
 
-
-(define (left-over total guess)
-  (- total (* guess guess)))
-
-(define (good-enough? total guess)
-   (> 0 (left-over total (+ guess 1))))
-
-(define (largest-square total guess)
-  (if (good-enough? total guess)
-      guess
-      (largest-square total (+ guess 1))))
-    
-
-(test "good-enough? returns true for 13 and 3"
-      (assert-true (good-enough? 13 3)))
-
-(test "good-enough? returns false for 13 and 2"
-      (assert-false (good-enough? 13 2)))
-
 (test "largest-square returns 3 for 13"
       (assert-equals 3 (largest-square 13 2)))
+
+(test "largest-square returns 4 for 17"
+      (assert-equals 4 (largest-square 17 2)))
+
+(define (largest-square total guess)
+ 
+  (define (good-enough?)
+    (< total (* (next-guess) (next-guess))))
+
+  (define (next-guess)
+    (+ guess 1))
+
+  (if (good-enough?)
+      guess
+      (largest-square total (next-guess))))
+
--- test-helpers.scm
+++ test-helpers.scm
@@ -1,13 +1,14 @@
 (define (assert-true a) a)
 (define (assert-false a) (not a))
 (define (assert-equals a b)
+  (display (word "  EXPECTED: " a)) 
+  (display (word "  ACTUAL: " b))
   (= a b))
 
 (define (test text assertion)
   (newline) (display (word "TEST: " text)) (newline)
   (if assertion
-      (display "  PASS")
-      (display "  FAIL"))
+      (display "    PASS")
+      (display "    FAIL"))
   (newline))
-
 
