3f0d51a061e97ba40504cc0e03d6f38055684802
Ben Tillett <ben@myrecovery.ai>
2017-03-19 09:30:12 +0000

    assert equals now handles strings lists words and numbers

--- test-helpers-tests.scm
+++ test-helpers-tests.scm
@@ -6,3 +6,17 @@
 (test "assert-equals returns false for two non matching numbers"
       (assert-false (assert-equals 12 13)))
 
+(test "assert-equals returns true for two matching words"
+      (assert-true (assert-equals 'sheep 'sheep)))
+      
+(test "assert-equals returns false for two non matching words"
+      (assert-false (assert-equals 'sheep 'dog)))
+
+(test "assert-equals returns true for two matching lists"
+      (assert-true (assert-equals '(sheep dog) '(sheep dog))))
+
+(test "assert-equals returns false for two non matching lists"
+      (assert-true (assert-equals '(sheep dog) '(sheep cat))))
+
+(test "assert-equals returns equal for different types"
+      (assert-false (assert-equals 5 'cake)))
--- test-helpers.scm
+++ test-helpers.scm
@@ -10,16 +10,34 @@
     (not (assertion))))
 
 (define (assert-equals a b)
-  (assert-equals-number a b))
 
-(define (assert-equals a b)
-  (define (assertion)
-    (display (word "\n  EXPECTED: " a))
-    (display (word "\n  ACTUAL: " b))
-    (= a b))
-  assertion)
+  (define (both-numbers?)
+    (and (number? a) (number? b)))
 
+  (define (both-words?)
+    (and (word? a) (word? b)))
+
+  (define (both-strings?)
+    (and (string? a) (string? b))) 
+
+  (define (both-lists?)
+    (and (list? a) (list? b)))
   
+  (lambda ()
+    (display "\n  EXPECTED: ")
+    (display a)
+    (display "\n  ACTUAL: ")
+    (display b)
+    (cond
+     ((both-numbers?)
+      (= a b))
+     ((or both-words? both-lists? both-strings?)
+      (equal? a b))
+     (else #f))))
+
+(define (assert-equals-number a b)
+  (= a b))
+
 (define (test text assertion)
   (display (word "\nTEST: " text))
   (if (assertion)
