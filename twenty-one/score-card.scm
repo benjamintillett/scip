5a68ffce4c4f0b51ec69178d2d2ff46e6367ac84
Ben Tillett <ben@myrecovery.ai>
2017-03-24 09:01:34 +0000

    complete score card method

--- twenty-one/score-card.scm
+++ twenty-one/score-card.scm
@@ -1,2 +1,17 @@
+(define NUMBERS '(2 3 4 5 6 7 8 9 10))
+(define FACES '(J Q K))
+(define ACE 'A)
+
 (define (score-card card)
-  1)
+  (define (rank)
+    (first card))
+  
+  (define (score-rank rank)
+    (cond
+     ((member? rank NUMBERS) rank)
+     ((member? rank FACES) 10)
+     ((equal? rank ACE) #f)))
+
+  (score-rank (rank)))
+    
+  
--- twenty-one/score-card.test.scm
+++ twenty-one/score-card.test.scm
@@ -3,3 +3,12 @@
 
 (test "score-card: returns one for one of spades"
       (assert-equals 1 (score-card '1S)))
+
+(test "score-card: returns two for two of hearts"
+      (assert-equals 2 (score-card '2S)))
+
+(test "score-card: returns 10 for jack of spades"
+      (assert-equals 10 (score-card 'JS)))
+
+(test "score-card: returns false for ace of hearts"
+      (assert-equals #f (score-card 'AH)))
