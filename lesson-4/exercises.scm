0d3aa0bdd4e8b8e3018893c82ae194758922725b
Ben Tillett <ben@myrecovery.ai>
2017-03-30 14:19:05 +0100

    add sum list and car list methods

--- lesson-4/exercises.scm
+++ lesson-4/exercises.scm
@@ -2,3 +2,6 @@
   (if(empty? list)
      0
      (+ (car list) (sum-list (cdr list)))))
+
+(define (car-list list)
+  (map car list))
--- lesson-4/exercises.test.scm
+++ lesson-4/exercises.test.scm
@@ -3,4 +3,15 @@
 (test "sum-list"
       (assert-equals 6 (sum-list '(1 2 3))))
 
+(test "sum-list"
+      (assert-equals 4 (sum-list '(0 -1 5))))
 
+(define lst (list (list 'hi 'hello)
+		  (list 'my 'cat) 
+                  (list 'name 'tag)
+                  (list 'is 'that) 
+                  (list 'adam 'smith)))
+
+
+(test "car-list: takes a list of lists and returns a list containing the first element of each"
+      (assert-equals '(hi my name is adam) (car-list lst)))
